b350486ad544d596f840e0cee1c2a4e4d6471c54
EJShim <soulrommel@gmail.com>
2019-11-04 17:23:00 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,38 +1,124 @@
 import React, {Component} from 'react';
-import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
+import PropTypes from 'prop-types';
+
+
+import { withStyles } from '@material-ui/styles';
+import AppBar from '@material-ui/core/AppBar';
+import { Toolbar, IconButton } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import Button from '@material-ui/core/Button';
-
+import MenuIcon from '@material-ui/icons/Menu';
+import { fade } from '@material-ui/core/styles';
 
 function Copyright() {
-  return (
-    <Typography variant="body2" color="primary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://ejshim.github.io/">
-        EJShim
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+    return (
+        <Typography variant="body2" color="primary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://ejshim.github.io/">
+            EJShim
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
 }
+
+
+const styles = theme => ({
+    grow: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      display: 'none',
+      [theme.breakpoints.up('sm')]: {
+        display: 'block',
+      },
+    },
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: fade(theme.palette.common.white, 0.25),
+      },
+      marginRight: theme.spacing(2),
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(3),
+        width: 'auto',
+      },
+    },
+    searchIcon: {
+      width: theme.spacing(7),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 7),
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        width: 200,
+      },
+    },
+    sectionDesktop: {
+      display: 'none',
+      [theme.breakpoints.up('md')]: {
+        display: 'flex',
+      },
+    },
+    sectionMobile: {
+      display: 'flex',
+      [theme.breakpoints.up('md')]: {
+        display: 'none',
+      },
+    },
+  });
+
 
 
 class App extends Component {
   
-  render(){
-    return (
-      <Container maxWidth="sm">
-        <Box my={4}>
-          <Typography variant="h4" component="h1" gutterBottom> Create React App v4-beta Example</Typography>
-          <Copyright />
-        </Box>
-      </Container>
-    );
-  }
-  
+
+    render(){
+
+        const { classes } = this.props;
+
+
+        return (
+            <div>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" className={classes.title}>
+                            News
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+
+                <Copyright/>
+            </div>
+        );
+    }
 }
 
-export default App;
+App.propTypes = {
+    classes : PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(App);
